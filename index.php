88a2feee6a0972635c0a5b1545bbcd399fc9a504
Dave Tesch <d@vetes.ch>
2020-09-30 09:41:19 -0500

    Static looking good

--- index.php
+++ index.php
@@ -5,22 +5,147 @@
   <meta charset="utf-8">
   <title>Ucare Banners</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="banners.css">
+	<script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
 </head>
-
 <body>
 
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
-  <script src="banner.js"></script>
+<div class="banner-container">
 
-  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-  <script>
-    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-    ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-  </script>
-  <script src="https://www.google-analytics.com/analytics.js" async></script>
+
+	<div class="ucareb" id="b970x250">
+		<div class="inner">
+			<div class="logo-text">
+				<img src="logo.svg" alt="ucare">
+			</div>
+			<div class="logo-spark">
+				<img src="spark.svg" alt="ucare">
+			</div>
+			<div class="text-jumper">
+				<span class="step-0">Medicare </span>
+				<span class="step-1">plans </span>
+				<span class="step-2">for </span>
+				<span class="step-3">every </span>
+				<span class="step-4">lifestyle </span>
+				<span class="step-5">and </span>
+				<span class="step-6">budget</span>
+			</div>
+			<span class="button">Shop now</span>
+		</div>
+	</div>
+
+	<div class="ucareb" id="b300x600">
+		<div class="inner">
+			<div class="logo-text">
+				<img src="logo.svg" alt="ucare">
+			</div>
+			<div class="logo-spark">
+				<img src="spark.svg" alt="ucare">
+			</div>
+			<div class="text-jumper">
+				<span class="step-0">Medicare </span>
+				<span class="step-1">plans </span>
+				<span class="step-2">for </span>
+				<span class="step-3">every </span>
+				<span class="step-4">lifestyle </span>
+				<span class="step-5">and </span>
+				<span class="step-6">budget</span>
+			</div>
+			<span class="button">Shop now</span>
+		</div>
+	</div>
+
+	<div class="ucareb" id="b160x600">
+		<div class="inner">
+			<div class="logo-text">
+				<img src="logo.svg" alt="ucare">
+			</div>
+			<div class="logo-spark">
+				<img src="spark.svg" alt="ucare">
+			</div>
+			<div class="text-jumper">
+				<span class="step-0">Medicare </span>
+				<span class="step-1">plans </span>
+				<span class="step-2">for </span>
+				<span class="step-3">every </span>
+				<span class="step-4">lifestyle </span>
+				<span class="step-5">and </span>
+				<span class="step-6">budget</span>
+			</div>
+			<span class="button">Shop now</span>
+		</div>
+	</div>
+
+	<div class="ucareb" id="b300x250">
+		<div class="inner">
+			<div class="logo-text">
+				<img src="logo.svg" alt="ucare">
+			</div>
+			<div class="logo-spark">
+				<img src="spark.svg" alt="ucare">
+			</div>
+			<div class="text-jumper">
+				<span class="step-0">Medicare </span>
+				<span class="step-1">plans </span>
+				<span class="step-2">for </span>
+				<span class="step-3">every </span>
+				<span class="step-4">lifestyle </span>
+				<span class="step-5">and </span>
+				<span class="step-6">budget</span>
+			</div>
+			<span class="button">Shop now</span>
+		</div>
+	</div>
+
+	<div class="ucareb" id="b728x90">
+		<div class="inner">
+			<div class="logo-text">
+				<img src="logo.svg" alt="ucare">
+			</div>
+			<div class="logo-spark">
+				<img src="spark.svg" alt="ucare">
+			</div>
+			<div class="text-jumper">
+				<span class="step-0">Medicare </span>
+				<span class="step-1">plans </span>
+				<span class="step-2">for </span>
+				<span class="step-3">every </span>
+				<span class="step-4">lifestyle </span>
+				<span class="step-5">and </span>
+				<span class="step-6">budget</span>
+			</div>
+			<span class="button">Shop now</span>
+		</div>
+	</div>
+
+	<div class="ucareb" id="b320x50">
+		<div class="inner">
+			<div class="logo-text">
+				<img src="logo.svg" alt="ucare">
+			</div>
+			<div class="logo-spark">
+				<img src="spark.svg" alt="ucare">
+			</div>
+			<div class="text-jumper">
+				<span class="step-0">Medicare </span>
+				<span class="step-1">plans </span>
+				<span class="step-2">for </span>
+				<span class="step-3">every </span>
+				<span class="step-4">lifestyle </span>
+				<span class="step-5">and </span>
+				<span class="step-6">budget</span>
+			</div>
+			<span class="button">Learn more</span>
+		</div>
+	</div>
+
+</div><!--/banner-container-->
+
+  <script src="banner.js"></script>
 </body>
 
 </html>
