c1a56ff00170cb0c222c7c886798a4310a78febd
Dave Tesch <d@vetes.ch>
2020-10-21 13:25:08 -0500

    restructure for round 2s

---
+++ round-2-dark/index.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>Ucare</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+  <style><?php include('../src/round-2-dark.css');?></style>
+	<script><?php include('../src/jquery-3.5.1.slim.min.js');?></script>
+</head>
+
+<body>
+
+  <div class="banner-container">
+
+  <a href="https://ucare.org/medicare">
+  	<div class="ucareb" id="b970x250" data-text-delay="1000" data-logo-delay="100">
+  		<div class="inner">
+  			<div class="logo-text">
+          <div class="logo-hide" data-logo-reveal="500"></div>
+  				<img src="logo.svg" alt="ucare">
+  			</div>
+  			<div class="logo-spark inactive">
+  				<img src="spark.svg" alt="ucare">
+  			</div>
+  			<div class="text-jumper inactive">
+          <span data-timeline="2000">Medicare </span>
+  				<span data-timeline="2500">plans </span>
+  				<span data-timeline="3000">for </span>
+  				<span data-timeline="3500">every </span>
+  				<span data-timeline="4000">lifestyle </span>
+  				<span data-timeline="4500">and </span>
+  				<span data-timeline="5000">budget</span>
+  			</div>
+  			<span class="button inactive">Shop now</span>
+  		</div>
+  	</div>
+  </a>
+
+  <a href="https://ucare.org/medicare">
+  	<div class="ucareb" id="b300x600" data-text-delay="1000" data-logo-delay="100">
+  		<div class="inner">
+  			<div class="logo-text">
+          <div class="logo-hide" data-logo-reveal="500"></div>
+  				<img src="logo.svg" alt="ucare">
+  			</div>
+  			<div class="logo-spark inactive">
+  				<img src="spark.svg" alt="ucare">
+  			</div>
+  			<div class="text-jumper inactive">
+          <span data-timeline="2000">Medicare </span>
+  				<span data-timeline="2500">plans </span>
+  				<span data-timeline="3000">for </span>
+  				<span data-timeline="3500">every </span>
+  				<span data-timeline="4000">lifestyle </span>
+  				<span data-timeline="4500">and </span>
+  				<span data-timeline="5000">budget</span>
+  			</div>
+  			<span class="button inactive">Shop now</span>
+  		</div>
+  	</div>
+  </a>
+
+  <a href="https://ucare.org/medicare">
+  	<div class="ucareb" id="b160x600" data-text-delay="1000" data-logo-delay="100">
+  		<div class="inner">
+  			<div class="logo-text">
+          <div class="logo-hide" data-logo-reveal="500"></div>
+  				<img src="logo.svg" alt="ucare">
+  			</div>
+  			<div class="logo-spark inactive">
+  				<img src="spark.svg" alt="ucare">
+  			</div>
+  			<div class="text-jumper inactive">
+          <span data-timeline="2000">Medicare </span>
+  				<span data-timeline="2500">plans </span>
+  				<span data-timeline="3000">for </span>
+  				<span data-timeline="3500">every </span>
+  				<span data-timeline="4000">lifestyle </span>
+  				<span data-timeline="4500">and </span>
+  				<span data-timeline="5000">budget</span>
+  			</div>
+  			<span class="button inactive">Shop now</span>
+  		</div>
+  	</div>
+  </a>
+
+  <a href="https://ucare.org/medicare">
+  	<div class="ucareb" id="b300x250" data-text-delay="1000" data-logo-delay="100">
+  		<div class="inner">
+  			<div class="logo-text">
+          <div class="logo-hide" data-logo-reveal="500"></div>
+  				<img src="logo.svg" alt="ucare">
+  			</div>
+  			<div class="logo-spark inactive">
+  				<img src="spark.svg" alt="ucare">
+  			</div>
+  			<div class="text-jumper inactive">
+          <span data-timeline="2000">Medicare </span>
+  				<span data-timeline="2500">plans </span>
+  				<span data-timeline="3000">for </span>
+  				<span data-timeline="3500">every </span>
+  				<span data-timeline="4000">lifestyle </span>
+  				<span data-timeline="4500">and </span>
+  				<span data-timeline="5000">budget</span>
+  			</div>
+  			<span class="button inactive">Shop now</span>
+  		</div>
+  	</div>
+  </a>
+
+  <a href="https://ucare.org/medicare">
+  	<div class="ucareb" id="b728x90" data-text-delay="1000" data-logo-delay="100">
+  		<div class="inner">
+  			<div class="logo-text">
+          <div class="logo-hide" data-logo-reveal="500"></div>
+  				<img src="logo.svg" alt="ucare">
+  			</div>
+  			<div class="logo-spark inactive">
+  				<img src="spark.svg" alt="ucare">
+  			</div>
+  			<div class="text-jumper inactive">
+          <span data-timeline="2000">Medicare </span>
+  				<span data-timeline="2500">plans </span>
+  				<span data-timeline="3000">for </span>
+  				<span data-timeline="3500">every </span>
+  				<span data-timeline="4000">lifestyle </span>
+  				<span data-timeline="4500">and </span>
+  				<span data-timeline="5000">budget</span>
+  			</div>
+  			<span class="button inactive">Shop now</span>
+  		</div>
+  	</div>
+  </a>
+
+  <a href="https://ucare.org/medicare">
+  	<div class="ucareb" id="b320x50" data-text-delay="1000" data-logo-delay="100">
+  		<div class="inner">
+  			<div class="logo-text">
+          <div class="logo-hide" data-logo-reveal="500"></div>
+  				<img src="logo.svg" alt="ucare">
+  			</div>
+  			<div class="logo-spark inactive">
+  				<img src="spark.svg" alt="ucare">
+  			</div>
+  			<div class="text-jumper inactive">
+          <span data-timeline="2000">Medicare </span>
+  				<span data-timeline="2500">plans </span>
+  				<span data-timeline="3000">for </span>
+  				<span data-timeline="3500">every </span>
+  				<span data-timeline="4000">lifestyle </span>
+  				<span data-timeline="4500">and </span>
+  				<span data-timeline="5000">budget</span>
+  			</div>
+  			<span class="button inactive">Shop now</span>
+  		</div>
+  	</div>
+  </a>
+
+</div>
+
+<script><?php include('../src/round-2-dark.js');?></script>
+</body>
+
+</html>
---
+++ src/round-2-light.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  var $banner = $('.ucareb');
+  $banner.each(function(){
+    var $thisBanner = $(this);
+    var $textDelay = $thisBanner.data('textDelay');
+    var $logoDelay = $thisBanner.data('logoDelay');
+
+    //logo text reveal
+    var $logoText = $thisBanner.find('.logo-text');
+    var $logoHide = $logoText.find('.logo-hide');
+    var $hideDuration = $logoHide.data('logoReveal');
+    setTimeout(function() {
+      $logoHide.toggleClass('active');
+    }, $hideDuration);
+
+    //text
+    var $textJumper = $thisBanner.find('.text-jumper');
+    var $button = $thisBanner.find('.button');
+    //reveal the text and button
+    setTimeout(function() {
+      $textJumper.toggleClass('inactive');
+      $button.toggleClass('inactive');
+    }, $textDelay);
+    $textJumper.each(function(){
+      var $word = $(this).find('span');
+      $word.each(function(){
+        var $this = $(this);
+        var $timeline = $this.data('timeline');
+        var $duration = $timeline + 500;
+        setTimeout(function() {
+          $this.toggleClass('active');
+        }, $timeline);
+        setTimeout(function() {
+          $this.toggleClass('active');
+        }, $duration);
+      });
+    });//tj each
+
+    //logo spark spin-in
+    setTimeout(function() {
+      $spark = $thisBanner.find('.logo-spark');
+      $spark.toggleClass('inactive');
+    }, $logoDelay);
+
+  });//banner each
+});
